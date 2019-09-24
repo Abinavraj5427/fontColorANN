cca4b3ef15a39fa3c151a2dc9f2fea1c78aefea6
OTHSCS106 <OTHSCS106@KATYISD.ORG>
2019-09-24 08:14:57 -0500

    Fixed AJAX problems. Having ERROR 404 for GET requests.

--- ajax.js
+++ ajax.js
@@ -1,7 +1,9 @@
-$('.button').click(function() {
+$('button').click(function(e) {
+    e.preventDefault()
     var R = $("#R").val();
     var G = $("#G").val();
     var B = $("#B").val();
+    $("#form").css("background-color", "rgb("+R+","+G+","+B+")");
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function(){
       if(this.readyState == 4 && this.status == 200){
--- predict.php
+++ predict.php
@@ -4,6 +4,8 @@
         $R = explode('-', $RGB,0);
         $G = explode('-', $RGB,1);
         $B = explode('-', $RGB,2)
+        
+        echo "<script>console.log("$R")</script>";
         echo shell_exec("python -c from fontANN.py import *; print predict("+R+","+G+","+B+")");
     }
 ?>
