b792fd0d51dc1b069a703cda6dbc15e0aba0fd7f
OTHSCS106 <OTHSCS106@KATYISD.ORG>
2019-09-30 08:15:24 -0500

    Need to add an output from shell_exec()

--- predict.php
+++ predict.php
@@ -2,5 +2,5 @@
         $R = $_POST["R"];
         $G = $_POST["G"];
         $B = $_POST["B"];
-        echo shell_exec("python -c from fontANN.py import *; print predict("+$R+","+$G+","+$B+")");
+        echo shell_exec("py -c 'from fontANN  import predict; print(predict("+$R+","+$G+","+$B+"))'");
 ?>
