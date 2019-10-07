6f8aa59a57a26ae2be2e1a8be5c0967c547a9c2d
OTHSCS106 <OTHSCS106@KATYISD.ORG>
2019-10-07 07:59:23 -0500

    Made minimal changes

--- predict.php
+++ predict.php
@@ -2,8 +2,8 @@
         $R = $_POST["R"];
         $G = $_POST["G"];
         $B = $_POST["B"];
-        $command = "py -c 'from fontANN import nada; print(nada())'";
+        // $command = "py -c 'from fontANN import predict; print(predict($R, $G, $B))'";
+        $command = "py test.py";
         $res = shell_exec($command);
         echo $res
-
 ?>
